53a9b8ecba1b0c56709e837b4159feb6adea8f2f
Danielra62 <74479354+Danielra62@users.noreply.github.com>
2024-09-29 19:47:27 -0500

    Add files via upload

---
+++ src/test/java/testRegistrarVacuna.java
@@ -0,0 +1,18 @@
+
+import com.company.forms.FormRegistrarVacunarRespaldo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class testRegistrarVacuna {
+    public static void main(String[] args) {
+        FormRegistrarVacunarRespaldo registrarVacuna=new FormRegistrarVacunarRespaldo();
+        registrarVacuna.setVisible(true);
+    }
+}
